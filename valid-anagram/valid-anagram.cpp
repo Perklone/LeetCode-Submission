3bef96d06f09150d9259e252b2fbf6d71f11b326
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2021-12-02 11:14:20 +0700

    Time: 4 ms (97.44%), Space: 7.3 MB (43.64%) - LeetHub

---
+++ valid-anagram/valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int x = s.length();
+        int y = t.length();
+        int arr[27];
+        int arr2[27];
+        if(x!=y) return false;
+        memset(arr, 0, sizeof(arr));
+        memset(arr2, 0, sizeof(arr2));
+        for(int i=0;i<x;i++){
+            arr[s[i] - 'a']++;
+            
+        } 
+         for(int a=0;a<y;a++){
+            arr2[t[a] - 'a']++;
+            
+        } 
+        for(int i=0;i<26;i++){
+            if(arr[i]!=arr2[i]){
+                return false;
+            }
+            else continue;
+        }
+    return true;
+        
+    }
+};
