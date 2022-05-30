e4718106a5d9499ae092a77b75a515952244da65
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2022-05-30 20:36:53 +0700

    Time: 105 ms (22.62%), Space: 14.7 MB (21.78%) - LeetHub

---
+++ 1-two-sum/1-two-sum.swift
@@ -0,0 +1,17 @@
+class Solution {
+    
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var result: [Int] = []
+       for i in 0..<nums.count {
+           for j in i+1..<nums.count {
+               if nums[i]+nums[j] == target{
+                   result.append(i)
+                   result.append(j)
+                   return result
+               }
+               
+           }
+       }
+     return result    
+    }
+}
