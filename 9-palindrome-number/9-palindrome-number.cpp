2a024e6d0c70058531a22fd6ef819d1a98d3fc71
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2022-08-03 15:01:18 +0700

    Time: 24 ms (46.93%), Space: 5.9 MB (74.58%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string str = to_string(x);
-        int len = str.length();
+        
         int start = 0;
-        int end = len-1;
+        int end = str.length() -1;
         while(start<end) {
             if(str[start] != str[end]) {
                 return false;
