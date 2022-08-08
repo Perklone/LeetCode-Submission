5560979ff8ab4d5691db60e45ab96dddf0839ab5
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2022-08-08 16:37:42 +0700

    Time: 14 ms (87.20%), Space: 8.9 MB (40.12%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int arr[27] = {0};
+        int check = 0;
+        for(int i=0;i<magazine.length();i++){
+            arr[magazine[i]-97]++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            int chara = ransomNote[i] - 97;
+            if(arr[chara] <= 0) {
+                return false;
+            }
+            else {
+                arr[chara]--;
+                check++;
+            }
+        }
+        return true;
+    }
+};
