0d01bbc8eb5c4fee547895ac0ab3a06f99ff55a7
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2022-08-18 14:04:49 +0700

    Time: 25 ms (85.57%), Space: 11.3 MB (50.41%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int>vec;
+        traverse(root,vec);
+        return vec;
+    }
+private:
+    void traverse(Node* root, vector<int>&vec) {
+        if(root == NULL) {
+            return;
+        }
+        int len = root->children.size();
+        vec.push_back(root->val);
+        for(int i =0;i<len;i++) {
+            traverse(root->children[i],vec);
+        }
+        
+    }
+};
