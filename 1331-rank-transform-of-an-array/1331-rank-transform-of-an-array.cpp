f1c90d75a83289315f2f638106a5640d85137143
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2022-08-31 17:32:54 +0700

    Time: 201 ms (41.70%), Space: 40.7 MB (42.87%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int len = arr.size();
+        vector<int> res;
+        vector<int> arr2 = arr;
+        sort(arr2.begin(),arr2.end());
+        int temp = 0;
+        int rank = 0;
+        unordered_map<int,int>um;
+        for(int i=0;i<len;i++ ){
+            if(arr2[i]!=temp) {
+                temp = arr2[i];
+                rank++;
+                um[arr2[i]] = rank;
+            }
+            else {
+                um[arr2[i]] = rank;
+            }
+        }
+        for(int i = 0;i<len;i++){
+            res.push_back(um[arr[i]]);
+        }
+        return res;
+    }
+};
