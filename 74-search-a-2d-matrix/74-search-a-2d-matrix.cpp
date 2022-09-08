9d6a379f7c20f245d5c4cb7761b5f7f41f466290
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2022-09-08 13:54:07 +0700

    Time: 0 ms (100.00%), Space: 9.6 MB (51.47%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int startrow = 0;
-        int startcol = 0;
-        int endrow = matrix.size()-1;
-        int endcol = matrix[0].size()-1;
-        
-        while(startrow <= endrow && startcol <= endcol) {
-            int midrow = startrow + (endrow-startrow) /2;
-            int midcol = startcol + (endcol-startcol) /2;
-
-            if(matrix[midrow][midcol] == target) {
+        int start = 0;
+        int end = matrix.size()*matrix[0].size()-1;
+        int check = matrix[0].size();
+        while(start<=end) {
+            int mid = start + (end-start)/2;
+            
+            if(matrix[mid/check][mid%check] == target) {
                 return true;
             }
-            else if(matrix[midrow][midcol]>target){
-                if(matrix[midrow][0]>target){
-                    
-                    endrow = midrow-1;
-                }
-                else{
-                    endcol = midcol-1;
-                }
-            }else{
-                if(matrix[midrow][matrix[0].size()-1]<target){
-                    startrow = midrow+1;
-                }
-                else{
-                    startcol = midcol+1;
-                }
+            else if(matrix[mid/check][mid%check] > target) {
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
             }
         }
-        
         return false;
     }
 };
