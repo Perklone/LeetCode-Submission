d9043ddfba1a9dfb76791da78f9f540655d930e2
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2022-09-21 10:28:21 +0700

    Time: 120 ms (87.10%), Space: 45.9 MB (78.57%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -5,11 +5,10 @@ public:
         
         int res = 0;
         vector<int> vec;
-        unordered_map<int,int>um;
+        
         for(int i = 0; i<nums.size();i++) {
             if(nums[i]%2 == 0) {
                 res+=nums[i];
-                um[i]++;
             }
         }
         
@@ -17,19 +16,16 @@ public:
             int temp = queries[i][0];
             int x = queries[i][1];
             
-            if((nums[x]+temp)%2 == 0 && um[x] == 0) {
-                
-                um[x]++;
+            if((nums[x]+temp)%2 == 0 && nums[x]%2 != 0) {
                 nums[x]+=temp;
                 
                 res+=nums[x];
             }
-            else if ((nums[x]+temp)%2 == 0 && um[x] != 0) {
+            else if ((nums[x]+temp)%2 == 0 && nums[x]%2 == 0) {
                 nums[x]+=temp;
                 res+=temp;
             }
-            else if((nums[x]+temp)%2 != 0 && um[x]!= 0) {
-                um[x]--;
+            else if((nums[x]+temp)%2 != 0 && nums[x]%2 == 0) {
                 res-=nums[x];
                 nums[x]+=temp;
                 
